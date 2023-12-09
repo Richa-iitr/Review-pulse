805f600adc592c1ed571451061473a74a4e1e5c0
lordforever <shashanktrivedi1917@gmail.com>
2023-12-09 23:20:12 +0530

    hooks updated

--- frontend/src/hooks/useUniversalContract.js
+++ frontend/src/hooks/useUniversalContract.js
@@ -4,7 +4,7 @@ import { useWeb3Contract } from "react-moralis";
 import useWeb3 from "./useWeb3";
 import { ethers } from "ethers";
 
-const useDealClient = () => {
+const useUniversalContract = () => {
   const { userAccount } = useWeb3();
   const { runContractFunction, data, error, isLoading } = useWeb3Contract({});
   const registerAadhar = async (nullifier) => {
@@ -87,6 +87,32 @@ const useDealClient = () => {
     }
   };
 
+  const review = async (amount, productId) => {
+    const parameters = {
+      abi: abi.universal,
+      contractAddress: contractAddress.universal,
+      functionName: "review",
+      params: {
+        productId: productId,
+      },
+      msgValue: ethers.utils.parseEther(amount),
+    };
+    try {
+      const response = await runContractFunction({
+        params: parameters,
+        onSuccess: (response) => {
+          console.log(response);
+        },
+        onError: (error) => {
+          console.log(error);
+        },
+      });
+      return response;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   const getProducts = async (productId) => {
     const parameters = {
       abi: abi.universal,
@@ -111,6 +137,61 @@ const useDealClient = () => {
       console.log(error);
     }
   };
-  return { registerAadhar, getProducts, addProduct, buyProduct };
+
+  function getPopNFT(productId, userAccount) {
+    const parameters = {
+      abi: abi.popNFT,
+      contractAddress: contractAddress.popNFT,
+      functionName: "getPopNFT",
+      params: {
+        productId: productId,
+        user: userAccount,
+      },
+    };
+    try {
+      const response = runContractFunction({
+        params: parameters,
+        onSuccess: (response) => {
+          console.log(response);
+        },
+        onError: (error) => {
+          console.log(error);
+        },
+      });
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  function getProductCount() {
+    const parameters = {
+      abi: abi.universal,
+      contractAddress: contractAddress.universal,
+      functionName: "productCount",
+      params: {},
+    };
+    try {
+      const response = runContractFunction({
+        params: parameters,
+        onSuccess: (response) => {
+          console.log(response);
+        },
+        onError: (error) => {
+          console.log(error);
+        },
+      });
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  return {
+    registerAadhar,
+    getProducts,
+    addProduct,
+    buyProduct,
+    getProductCount,
+    getPopNFT,
+  };
 };
-export default useDealClient;
+export default useUniversalContract;
